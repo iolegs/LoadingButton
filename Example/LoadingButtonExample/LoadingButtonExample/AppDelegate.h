0f8ef26a839f271d1f4fc012114352cd165f625c
Oleg Sukhotskiy <osukhotskiy@diasoft.ru>
2017-03-31 01:20:38 +0300

    initial

---
+++ Example/LoadingButtonExample/LoadingButtonExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LoadingButtonExample
+//
+//  Created by Oleg Sukhotskiy on 31.03.17.
+//  Copyright © 2017 Oleg Sukhotskiy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
